768a0cd6e3bb0cdfa054d744bdd2e40ea153b3e3
Marissa Sherman <bytewisemarissa@gmail.com>
2023-11-11 14:34:09 -0500

    Tap color

--- src/HelpfulThings.Connect.Scryfall/Enums/Colors.cs
+++ src/HelpfulThings.Connect.Scryfall/Enums/Colors.cs
@@ -5,10 +5,11 @@ namespace HelpfulThings.Connect.Scryfall.Enums;
 
 public enum Colors
 {
-    [EnumMember(Value = "W")] [Description("Plains")] White,
-    [EnumMember(Value = "U")] [Description("Island")] Blue,
-    [EnumMember(Value = "B")] [Description("Swamp")] Black,
-    [EnumMember(Value = "R")] [Description("Mountain")] Red,
-    [EnumMember(Value = "G")] [Description("Forest")] Green,
-    [EnumMember(Value = "C")] [Description("Colorless")] Colorless,
+    [EnumMember(Value = "W")] [Description("Plains")] White = 1,
+    [EnumMember(Value = "U")] [Description("Island")] Blue = 2,
+    [EnumMember(Value = "B")] [Description("Swamp")] Black = 3,
+    [EnumMember(Value = "R")] [Description("Mountain")] Red = 4,
+    [EnumMember(Value = "G")] [Description("Forest")] Green = 5,
+    [EnumMember(Value = "C")] [Description("Colorless")] Colorless = 6,
+    [EnumMember(Value = "T")] [Description("Tap")] Tap = 7,
 }
