b71154a6caf865ab41f616c80ef50f5bf4ddaafd
Marissa Sherman <bytewisemarissa@gmail.com>
2023-11-10 21:45:35 -0500

    adding promotype serialized

--- src/HelpfulThings.Connect.Scryfall/Enums/PromoTypes.cs
+++ src/HelpfulThings.Connect.Scryfall/Enums/PromoTypes.cs
@@ -37,4 +37,5 @@ public enum PromoTypes
     [EnumMember(Value = "judgegift")] [Description("Judge Gift")] JudgeGift = 28,
     [EnumMember(Value = "embossed")] [Description("Embossed")] Embossed = 29,
     [EnumMember(Value = "instore")] [Description("In Store")] InStore = 30,
+    [EnumMember(Value = "serialized")] [Description("In Store")] Serialized = 31,
 }
