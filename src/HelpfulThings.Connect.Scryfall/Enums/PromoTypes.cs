adcc85f91084cdc44bceddcdb504a4368ab1223c
Marissa Sherman <bytewisemarissa@gmail.com>
2023-11-11 00:52:50 -0500

    full art

--- src/HelpfulThings.Connect.Scryfall/Enums/PromoTypes.cs
+++ src/HelpfulThings.Connect.Scryfall/Enums/PromoTypes.cs
@@ -44,5 +44,4 @@ public enum PromoTypes
     [EnumMember(Value = "wizardsplaynetwork")] [Description("Wizard's Play Network")] WizardsPlayNetwork = 35,
     [EnumMember(Value = "boxtopper")] [Description("Box Topper")] boxtopper = 36,
     [EnumMember(Value = "stepandcompleat")] [Description("Step and Compleat")] StepAndCompleat = 37,
-    [EnumMember(Value = "fullart")] [Description("Full Art")] FullArt = 37,
 }
